257adbfc2f763ac4607977a4b4c8077fd5163985
taki <nbjwl@live.com>
2021-06-27 11:24:25 +0800

    web demo

---
+++ web-demo/main.tf
@@ -0,0 +1,151 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "azure-learning"
+  location = "Korea Central"
+}
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "vn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "sg" {
+  name                = "learning_sg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = "vmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_B1ls"
+  instances           = 1
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "ssni"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.internal.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+    }
+
+    network_security_group_id = azurerm_network_security_group.sg.id
+  }
+
+  boot_diagnostics {}
+
+  custom_data = data.template_cloudinit_config.config.rendered
+}
+
+data "template_file" "script" {
+  template = file("cloud-init")
+}
+
+data "template_cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+
+  # Main cloud-config configuration file.
+  part {
+    filename     = "cloud-init"
+    content_type = "text/cloud-config"
+    content      = data.template_file.script.rendered
+  }
+}
+
+resource "azurerm_public_ip" "lb_pub" {
+  name                = "lb_pub"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb_pub.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+ loadbalancer_id     = azurerm_lb.lb.id
+ name                = "bpepool"
+}
+
+resource "azurerm_lb_probe" "probe" {
+ resource_group_name = azurerm_resource_group.rg.name
+ loadbalancer_id     = azurerm_lb.lb.id
+ name                = "lb_probe"
+ port                = 80
+}
+
+resource "azurerm_lb_rule" "rule" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.probe.id
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+}
